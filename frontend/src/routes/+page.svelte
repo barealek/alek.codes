1beef178ad56309e10829c43c509946fb58895b4
Alek <92643775+barealek@users.noreply.github.com>
2023-12-01 22:28:12 +0100

    Update navbar and the page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,45 @@
 <div class="h-screen w-screen flex justify-center items-center">
     <div>
-        <div class="relative flex justify-center">
-            <div class="w-60 h-60">
+        <!-- Avatar -->
+        <div class="relative flex justify-center items-center">
+            <div class="relative w-60 h-60">
                 <img class="object-cover w-full h-full rounded-full" src="/assets/me.jpg" alt="alek">
-                <span class="absolute bottom-7 w-5 h-5 rounded-full bg-emerald-500 right-7 ring-1 ring-white"></span>
+                <span class="absolute bottom-6 right-6 w-5 h-5 rounded-full bg-emerald-500 ring-1 ring-white"></span>
             </div>
         </div>
 
+        <!-- Name & title -->
         <div class="text-center">
             <h1 class="text-4xl font-bold mt-4">Aleksander Rist</h1>
             <p class="text-gray-500">Software Developer</p>
         </div>
+
+        <div class="flex gap-2 items-center justify-center mt-6">
+            <button class="rounded-sm flex items-center px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 gap-x-3 hover:bg-gray-100">
+                <svg class="w-5 h-5 sm:w-6 sm:h-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/><script xmlns=""/></svg>
+        
+                <span>Github</span>
+            </button>
+
+            <button class="rounded-sm flex items-center px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 gap-x-3 hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 sm:w-6 sm:h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                </svg>
+        
+        
+                <span>Discord</span>
+            </button>
+
+            <button class="rounded-sm flex items-center px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 gap-x-3 hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 sm:w-6 sm:h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                </svg>
+        
+                <span>Email</span>
+            </button>
+
+            
+        </div>
+        
     </div>
 </div>
