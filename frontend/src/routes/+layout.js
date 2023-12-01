9c3d8b79393eb2fb84a4748e2931f10f8a8fac1c
Alek <92643775+barealek@users.noreply.github.com>
2023-12-01 22:04:15 +0100

    Begin on frontend w/ svelte & tailwind

---
+++ frontend/src/routes/+layout.js
@@ -0,0 +1,9 @@
+
+// Prerender the page so it can be statically exported and served via CDN
+export const prerender = true
+
+// Disable server-side rendering globally
+export const ssr = false
+
+// Enable trailing slashes for all paths, so pages are directly accessible
+export const trailingSlash = 'always'
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="h-screen w-screen flex justify-center items-center">
+    <div>
+        <div class="relative flex justify-center">
+            <div class="w-60 h-60">
+                <img class="object-cover w-full h-full rounded-full" src="/assets/me.jpg" alt="alek">
+                <span class="absolute bottom-7 w-5 h-5 rounded-full bg-emerald-500 right-7 ring-1 ring-white"></span>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <h1 class="text-4xl font-bold mt-4">Aleksander Rist</h1>
+            <p class="text-gray-500">Software Developer</p>
+        </div>
+    </div>
+</div>
