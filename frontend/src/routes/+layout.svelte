c3360c86a3cd8e2b6449956a564a1dd6064ee76f
Alek <92643775+barealek@users.noreply.github.com>
2023-12-01 23:25:54 +0100

    Enable view transition api

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,20 @@
     import "../app.css"
 
     import Navbar from "$lib/components/Navbar.svelte";
+
+    import { onNavigate } from '$app/navigation';
+
+    onNavigate((navigation) => {
+        if (!document.startViewTransition) return;
+
+        return new Promise((resolve) => {
+            document.startViewTransition(async () => {
+                resolve();
+                await navigation.complete;
+            });
+        });
+    });
+
 </script>
 
 <Navbar/>
