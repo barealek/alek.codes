51c0d76b93a4c331e2454a59b8d26fd5ac431418
Alek <92643775+barealek@users.noreply.github.com>
2023-12-01 23:25:43 +0100

    Update navbar

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,9 +1,22 @@
+<script>
+    let activeClasses = "border-b-2 border-blue-500"
+    let unactiveClasses = "border-b-2 border-transparent hover:border-blue-500"
+
+    let path = window.location.pathname
+    let activePage = path
+    
+    let pages = {
+        home: "/",
+        blogs: "/blogs/",
+        contact: "/contact/"
+    }
+
+</script>
 <nav class="absolute w-screen bg-white shadow-sm transition-all hover:shadow dark:bg-gray-800 flex mb-8">
     <div class="container flex items-center justify-center p-6 mx-auto text-gray-600">
-        <a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
 
-        <a href="/blogs" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">blogs</a>
-
-        <a href="/contact" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">contact</a>
+        {#each Object.entries(pages) as [key, value]}
+            <a on:click={() => {activePage = value}} href={value} class="{activePage === value ? activeClasses : unactiveClasses} hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 mx-1.5 sm:mx-6">{key}</a>
+        {/each}
     </div>
 </nav>
