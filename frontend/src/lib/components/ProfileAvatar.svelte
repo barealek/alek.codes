d9a369b166dcb2acdcd1c5bb4d3cb689e2f5f398
Alek <92643775+barealek@users.noreply.github.com>
2023-12-02 11:05:30 +0100

    Move profile avatar to component

---
+++ frontend/src/lib/components/ProfileAvatar.svelte
@@ -0,0 +1,13 @@
+<div class="relative flex justify-center items-center">
+    <div class="relative w-60 h-60">
+        <img class="object-cover w-full h-full rounded-full" src="/assets/me.jpg" alt="alek">
+        <span class="absolute bottom-6 right-6 w-5 h-5 rounded-full bg-emerald-500 ring-1 ring-white"></span>
+    </div>
+</div>
+
+<!-- Name & title -->
+<div class="text-center">
+    <h1 class="text-4xl font-bold mt-4">Aleksander Rist <span class="text-lg font-normal">(16 år)</span></h1>
+    <p class="text-amber-600/80">Selvlært Software Udvikler</p>
+</div>
+
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,11 @@
+<script>
+    import ProfileAvatar from "$lib/components/ProfileAvatar.svelte";
+</script>
+
 <div class="h-screen w-screen flex justify-center items-center">
     <div>
         <!-- Avatar -->
-        <div class="relative flex justify-center items-center">
-            <div class="relative w-60 h-60">
-                <img class="object-cover w-full h-full rounded-full" src="/assets/me.jpg" alt="alek">
-                <span class="absolute bottom-6 right-6 w-5 h-5 rounded-full bg-emerald-500 ring-1 ring-white"></span>
-            </div>
-        </div>
-
-        <!-- Name & title -->
-        <div class="text-center">
-            <h1 class="text-4xl font-bold mt-4">Aleksander Rist <span class="text-lg font-normal">(16 år)</span></h1>
-            <p class="text-amber-600/80">Selvlært Software Udvikler</p>
-        </div>
+        <ProfileAvatar />
 
         <div class="flex gap-2 items-center justify-center mt-6">
             <a href="https://github.com/barealek" target="_blank" class="rounded-md flex items-center px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 gap-x-3 hover:bg-gray-100">
