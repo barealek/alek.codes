d8b4ae7e09d14a717783670bbafd78bbe10d3bb0
Alek <92643775+barealek@users.noreply.github.com>
2023-12-24 16:04:51 +0100

    Create portfolio.js

---
+++ frontend/src/lib/data/portfolio.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'StyledLogger',
+        type: 'Python Package',
+        description: "StyledLogger er et advanceret logging library til Python 3.9 og opad, som gør det muligt at lave flotte logs i terminalen og filer uden besvær.",
+        text: '',
+        image: '/assets/images/StyledLogger-Eksempel.png',
+        date: "April 2023"
+    },
+    {
+        title: 'Anti Toxicity',
+        type: 'Hjemmeside & Bot',
+        description: "Anti Toxicity er en Discord bot, der bruger kunstig intelligens til at scanne beskeder på Discord for hadfuld, voldelig og truende beskeder.",
+        text: 'This is the text for project 1.',
+        image: '/assets/images/AntiToxicity-Website.png',
+        date: "April 2022 - Nutid"
+    }
+]
