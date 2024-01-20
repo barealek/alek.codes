f76a928d76ab8a5da375f3c0ec869c46c3c7c50d
Alek <92643775+barealek@users.noreply.github.com>
2024-01-20 17:23:05 +0100

    Add PlexHost Katalog to portfolio

--- frontend/src/lib/data/portfolio.js
+++ frontend/src/lib/data/portfolio.js
@@ -1,5 +1,15 @@
 export default [
     {
+        title: "PlexHost Katalog",
+        type: "Hjemmeside & API",
+        description: "PlexHost Katalog er et pluginkatalog eksklusivt til PlexHost servere. Siden er lavet med Svelte og Go, og er en del af PlexHost's nye infrastruktur.",
+        image: "/assets/images/PlexHost-Katalog.png",
+        date: "Januar 2024",
+        links: {
+            hjemmeside: "https://katalog.plexhost.dk",
+        }
+    },
+    {
         title: 'StyledLogger',
         type: 'Python Library',
         description: "StyledLogger er et advanceret logging library til Python 3.9 og opad, som gør det muligt at lave flotte logs i terminalen og filer uden besvær, med avancerede funktioner som callbacks, levels mm.",
@@ -12,9 +22,9 @@ export default [
     {
         title: 'Anti Toxicity',
         type: 'Hjemmeside & Bot',
-        description: "Anti Toxicity er en Discord bot, der bruger kunstig intelligens til at scanne beskeder på Discord for hadfuld, voldelig og truende beskeder.",
+        description: "Anti Toxicity er en Discord bot, der bruger kunstig intelligens til at scanne beskeder på Discord for hadfuld, voldelig og truende beskeder. Bot og hjemmeside er lavet med Python.",
         image: '/assets/images/AntiToxicity-Website.png',
-        date: "April 2022 - Nutid",
+        date: "April 2022",
         links: {
             hjemmeside: 'https://antitoxicity.tech',
         }
